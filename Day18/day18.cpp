43941ae5504906928fa88f6cb410ecd6d24a4631
MattR2718 <66754574+MattR2718@users.noreply.github.com>
2022-12-18 14:20:59 +0000

    Clean Up Day18

--- Day18/day18.cpp
+++ Day18/day18.cpp
@@ -9,10 +9,9 @@
 #include <sstream>
 
 auto getInput(){
-    std::ifstream file("../../../../Day18/test.txt");
     std::string linetxt;
     std::vector<std::tuple<int, int,int>> out;
-    while (std::getline(file, linetxt)){
+    while (std::getline(std::cin, linetxt)){
         std::stringstream ss(linetxt);
         int x, y, z;
         char c1, c2, c3;
@@ -22,27 +21,25 @@ auto getInput(){
     return out;
 }
 
+auto getSurround(const std::tuple<int, int, int>& a, std::vector<std::tuple<int, int, int>>& b){
+    //std::vector<std::tuple<int, int, int>> all;
+    b.emplace_back(std::tuple(std::get<0>(a) + 1, std::get<1>(a), std::get<2>(a)));
+    b.emplace_back(std::tuple(std::get<0>(a) - 1, std::get<1>(a), std::get<2>(a)));
+    b.emplace_back(std::tuple(std::get<0>(a), std::get<1>(a) + 1, std::get<2>(a)));
+    b.emplace_back(std::tuple(std::get<0>(a), std::get<1>(a) - 1, std::get<2>(a)));
+    b.emplace_back(std::tuple(std::get<0>(a), std::get<1>(a), std::get<2>(a) + 1));
+    b.emplace_back(std::tuple(std::get<0>(a), std::get<1>(a), std::get<2>(a) - 1));
+}
+
 template<typename T>
 auto run1(T input){
     std::vector<std::tuple<int, int, int>> all;
 
     for(auto& i : input){
-        all.emplace_back(std::tuple(std::get<0>(i) + 1, std::get<1>(i), std::get<2>(i)));
-        all.emplace_back(std::tuple(std::get<0>(i) - 1, std::get<1>(i), std::get<2>(i)));
-        all.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i) + 1, std::get<2>(i)));
-        all.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i) - 1, std::get<2>(i)));
-        all.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i) + 1));
-        all.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i) - 1));
+        getSurround(i, all);
     }
 
     all.erase(std::remove_if(all.begin(), all.end(), [&](const auto& a){ return std::find(input.begin(),  input.end(), a) != input.end(); }), all.end());
-
-    /* for(int i = 0; i < all.size(); i++){
-        if(std::find(input.begin(), input.end(), all[i]) != input.end()){
-            all.erase(all.begin() + i--);
-        }
-    } */
-
 
     return all.size();
 }
@@ -51,28 +48,16 @@ void getVals(std::vector<std::tuple<int, int, int>>& all, const std::vector<std:
     if(std::get<0>(curr) < -1 || std::get<0>(curr) > std::get<0>(max)
         || std::get<1>(curr) < -1 || std::get<1>(curr) > std::get<1>(max)
         || std::get<2>(curr) < -1 || std::get<2>(curr) > std::get<2>(max)
-        || std::find(input.begin(), input.end(), curr) != input.end()
-        /* || std::find(all.begin(), all.end(), curr) != all.end() */ ){ /* std::cout<<"RETURNING\n"; */ return; }
-
-    //std::cout<<"CURR: "<<std::get<0>(curr) << ' '<< std::get<1>(curr) << ' '<< std::get<2>(curr)<<'\n';
+        || std::find(input.begin(), input.end(), curr) != input.end()){ return; }
 
     std::vector<std::tuple<int, int, int>> ps;
-    ps.emplace_back(std::tuple(std::get<0>(curr) + 1, std::get<1>(curr), std::get<2>(curr)));
-    ps.emplace_back(std::tuple(std::get<0>(curr) - 1, std::get<1>(curr), std::get<2>(curr)));
-    ps.emplace_back(std::tuple(std::get<0>(curr), std::get<1>(curr) + 1, std::get<2>(curr)));
-    ps.emplace_back(std::tuple(std::get<0>(curr), std::get<1>(curr) - 1, std::get<2>(curr)));
-    ps.emplace_back(std::tuple(std::get<0>(curr), std::get<1>(curr), std::get<2>(curr) + 1));
-    ps.emplace_back(std::tuple(std::get<0>(curr), std::get<1>(curr), std::get<2>(curr) - 1));
+    getSurround(curr, ps);
     
     ps.erase(std::remove_if(ps.begin(), ps.end(), [&](const auto& a){ return std::find(all.begin(),  all.end(), a) != all.end(); }), ps.end());
     ps.erase(std::remove_if(ps.begin(), ps.end(), [&](const auto& a){ return std::find(input.begin(),  input.end(), a) != input.end(); }), ps.end());
-    
-    //std::cout<<"PS SIZE: "<<ps.size()<<'\n';
-    
+        
     all.insert(all.end(), ps.begin(), ps.end());
     for(auto& i : ps){
-        //std::cout<<"I: "<<std::get<0>(i) << ' '<< std::get<1>(i) << ' '<< std::get<2>(i)<<'\n';
-        //all.push_back(i);
         getVals(all, input, i, max);
     }
 }
@@ -88,52 +73,24 @@ auto run2(T input){
         maxz = (std::get<2>(i) > maxz) ? std::get<2>(i) : maxz;
     }
 
-    std::cout<<'\n';
-    std::cout<<"MAXX: "<<maxx<<'\n';
-    std::cout<<"MAXY: "<<maxy<<'\n';
-    std::cout<<"MAXZ: "<<maxz<<'\n';
-
-    /* maxx += 1;
-    maxy += 1;
-    maxz += 1; */
-
-    /*
-    All values positive and 0, 0, 0 not a value
-    */
-
     std::vector<std::tuple<int, int, int>> all;
     std::tuple<int, int, int> O{0, 0, 0};
     std::tuple<int, int, int> max{maxx, maxy, maxz};
 
     getVals(all, input, std::tuple(0, 0, 0), std::tuple{maxx+1,maxy+1,maxz+1});
-    std::cout<<all.size()<<'\n';
 
     std::vector<std::tuple<int, int, int>> borders;
 
     for(auto& i : input){
-        borders.emplace_back(std::tuple(std::get<0>(i) + 1, std::get<1>(i), std::get<2>(i)));
-        borders.emplace_back(std::tuple(std::get<0>(i) - 1, std::get<1>(i), std::get<2>(i)));
-        borders.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i) + 1, std::get<2>(i)));
-        borders.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i) - 1, std::get<2>(i)));
-        borders.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i) + 1));
-        borders.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i) - 1));
+        getSurround(i, borders);
     }
 
     all.erase(std::remove_if(all.begin(), all.end(), [&](const auto& a){ return std::find(borders.begin(),  borders.end(), a) == borders.end(); }), all.end());
-    //auto a = std::unique(all.begin(), all.end());
     int count = 0;
     for(auto& i : all){
          std::vector<std::tuple<int, int, int>> sa;
-        sa.emplace_back(std::tuple(std::get<0>(i) + 1, std::get<1>(i), std::get<2>(i)));
-        sa.emplace_back(std::tuple(std::get<0>(i) - 1, std::get<1>(i), std::get<2>(i)));
-        sa.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i) + 1, std::get<2>(i)));
-        sa.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i) - 1, std::get<2>(i)));
-        sa.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i) + 1));
-        sa.emplace_back(std::tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i) - 1)); 
+        getSurround(i, sa);
         count += std::count_if(sa.begin(), sa.end(), [&](auto& a){ return std::find(input.begin(), input.end(), a) != input.end(); });
-        /* for(auto& s : sa){
-            if(std::find(input.begin(), input.end(), s) != input.end()){ count++; }
-        } */
     }
 
     return count;
@@ -151,5 +108,20 @@ int main(){
 }
 
 //Part 1: 3636
+//Part 2: 2102
 
-//2094 low
+/*
+RELEASE: 
+
+    Days              : 0
+    Hours             : 0
+    Minutes           : 0
+    Seconds           : 0
+    Milliseconds      : 534
+    Ticks             : 5340036
+    TotalHours        : 0.000148334333333333
+    TotalMinutes      : 0.00890006
+    TotalSeconds      : 0.5340036
+    TotalMilliseconds : 534.0036
+
+*/
